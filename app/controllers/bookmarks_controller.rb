c62f383c8d06ae4445548f4fd9229a09aa924d33
Yulia Mikhaylova ymik0410@gmail.com <ymik0410@gmail.com>
2021-04-15 23:16:17 +0200

    controllers and routes

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,36 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+      @bookmark = Bookmark.new
+      @list = List.find(params[:list_id])
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmarks_params)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list)
+    else
+      render :new
+    end
+
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+
+    # redirect_to
+  end
+
+  private
+
+  def bookmarks_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'lists/index'
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # get 'lists/:id', to: 'lists#show'
+  # post 'lists', to: 'lists#create'
+  resources :lists, only: [ :index, :new, :show, :create] do
+    resources :bookmarks, only: [ :index, :new, :create ]
+  end
+
+  resources :bookmarks, only: :destroy
 end
